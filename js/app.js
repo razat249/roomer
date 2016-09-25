132e4496c5989b4d83e221e13ee1a26da013d498
Rajat <razat249@gmail.com>
2016-09-25 22:54:33 +0530

    add sliding nav

--- js/app.js
+++ js/app.js
@@ -18,7 +18,11 @@ var initMap = function() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.7413549, lng: -73.9980244},
-    zoom: 13
+    zoom: 13,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+        position: google.maps.ControlPosition.TOP_RIGHT
+    }
   });
   // These are the real estate listings that will be shown to the user.
   // Normally we'd have these in a database instead.
@@ -80,6 +84,16 @@ function toggleBounce(marker) {
     }
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
 
 var stringStartsWith = function (string, startsWith) {          
